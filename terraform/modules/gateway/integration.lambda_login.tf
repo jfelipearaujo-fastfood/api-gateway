b18e2992da706400e7ca08ce1ccbb4ec91a859e7
Jose Felipe Araujo <j.felipe.araujo@outlook.com>
2024-03-18 22:26:07 -0300

    feat: :sparkles: change name of lambdas

--- terraform/modules/gateway/integration.lambda_login.tf
+++ terraform/modules/gateway/integration.lambda_login.tf
@@ -1,13 +1,13 @@
 
-resource "aws_api_gateway_resource" "proxy_lambda_auth" {
+resource "aws_api_gateway_resource" "proxy_lambda_login" {
   rest_api_id = aws_api_gateway_rest_api.main.id
   parent_id   = aws_api_gateway_rest_api.main.root_resource_id
-  path_part   = "auth"
+  path_part   = "login"
 }
 
-resource "aws_api_gateway_method" "proxy_lambda_auth" {
+resource "aws_api_gateway_method" "proxy_lambda_login" {
   rest_api_id   = aws_api_gateway_rest_api.main.id
-  resource_id   = aws_api_gateway_resource.proxy_lambda_auth.id
+  resource_id   = aws_api_gateway_resource.proxy_lambda_login.id
   http_method   = "ANY"
   authorization = "NONE"
 
@@ -17,10 +17,10 @@ resource "aws_api_gateway_method" "proxy_lambda_auth" {
   }
 }
 
-resource "aws_api_gateway_method_response" "proxy_lambda_auth" {
+resource "aws_api_gateway_method_response" "proxy_lambda_login" {
   rest_api_id = aws_api_gateway_rest_api.main.id
-  resource_id = aws_api_gateway_resource.proxy_lambda_auth.id
-  http_method = aws_api_gateway_method.proxy_lambda_auth.http_method
+  resource_id = aws_api_gateway_resource.proxy_lambda_login.id
+  http_method = aws_api_gateway_method.proxy_lambda_login.http_method
   status_code = "200"
 
   response_parameters = {
@@ -30,11 +30,11 @@ resource "aws_api_gateway_method_response" "proxy_lambda_auth" {
   }
 }
 
-resource "aws_api_gateway_integration_response" "proxy_lambda_auth" {
+resource "aws_api_gateway_integration_response" "proxy_lambda_login" {
   rest_api_id = aws_api_gateway_rest_api.main.id
-  resource_id = aws_api_gateway_resource.proxy_lambda_auth.id
-  http_method = aws_api_gateway_method.proxy_lambda_auth.http_method
-  status_code = aws_api_gateway_method_response.proxy_lambda_auth.status_code
+  resource_id = aws_api_gateway_resource.proxy_lambda_login.id
+  http_method = aws_api_gateway_method.proxy_lambda_login.http_method
+  status_code = aws_api_gateway_method_response.proxy_lambda_login.status_code
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
@@ -43,31 +43,31 @@ resource "aws_api_gateway_integration_response" "proxy_lambda_auth" {
   }
 
   depends_on = [
-    aws_api_gateway_method.proxy_lambda_auth,
-    aws_api_gateway_integration.lambda_auth,
+    aws_api_gateway_method.proxy_lambda_login,
+    aws_api_gateway_integration.lambda_login,
   ]
 }
 
-data "aws_lambda_function" "lambda_auth" {
-  function_name = "lambda_auth"
+data "aws_lambda_function" "lambda_login" {
+  function_name = "lambda_login"
 }
 
-resource "aws_lambda_permission" "api_gtw_lambda_auth_permission" {
+resource "aws_lambda_permission" "api_gtw_lambda_login_permission" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = data.aws_lambda_function.lambda_auth.function_name
+  function_name = data.aws_lambda_function.lambda_login.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.main.execution_arn}/*/*/*"
 }
 
-resource "aws_api_gateway_integration" "lambda_auth" {
+resource "aws_api_gateway_integration" "lambda_login" {
   rest_api_id = aws_api_gateway_rest_api.main.id
-  resource_id = aws_api_gateway_resource.proxy_lambda_auth.id
+  resource_id = aws_api_gateway_resource.proxy_lambda_login.id
   http_method = "ANY"
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = data.aws_lambda_function.lambda_auth.invoke_arn
+  uri                     = data.aws_lambda_function.lambda_login.invoke_arn
   passthrough_behavior    = "WHEN_NO_MATCH"
   content_handling        = "CONVERT_TO_TEXT"
 
--- terraform/modules/gateway/integration.lambda_register.tf
+++ terraform/modules/gateway/integration.lambda_register.tf
@@ -2,7 +2,7 @@
 resource "aws_api_gateway_resource" "proxy_lambda_register" {
   rest_api_id = aws_api_gateway_rest_api.main.id
   parent_id   = aws_api_gateway_rest_api.main.root_resource_id
-  path_part   = "users"
+  path_part   = "register"
 }
 
 resource "aws_api_gateway_method" "proxy_lambda_register" {
